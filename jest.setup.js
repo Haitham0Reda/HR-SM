e4b06c4510d55c4bac6a02929db83b60a52a0940
Haitham Mohamed Reda <102927584+Haitham0Reda@users.noreply.github.com>
2025-11-08 20:25:32 +0200

    Remove test files and update package files

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-// jest.setup.js
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './.env' });
-
-// Mock MongoDB connection for tests
-process.env.MONGO_URI = 'mongodb://localhost:27017/hrsm_test';
-process.env.JWT_SECRET = 'kwW3sBhgZDk%WsNp';
-process.env.PORT = '5001';
